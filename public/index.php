130704ece2c8b24fe466c467f25a64640d195c5f
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2020-03-26 17:30:01 -0400

    Remove CORS no arquivo public/index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,5 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: Application/json');
-header('Access-Control-Allow-Headers:  Content-Type, Origin');
-
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
