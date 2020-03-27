1e14a447f8faeeaaff30206c151b947b4d798e03
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2020-03-27 11:01:42 -0400

    Configura seeders para popular banco de dados

--- database/seeds/PersonSeeder.php
+++ database/seeds/PersonSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Person;
+use App\Photo;
 
 class PersonSeeder extends Seeder
 {
@@ -11,6 +13,30 @@ class PersonSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $count = (int)$this->command->ask('How to people do you need? (default 5)', 5);
+
+        if ($count < 5) {
+            $count = 5;
+            $this->command->info("The minor value for number of person need be {$count}.");
+        };
+
+        $this->command->info("Creating {$count} example people.");
+
+        $examplePeople = [
+            'Linus Torvalds',
+            'Bill Gates',
+            'Steve Wozniak',
+            'Tim Berners Lee',
+            'Elon Musk',
+        ];
+
+        for ($i=0; $i <= 4; $i++) {
+            $id = $i + 1;
+            Person::create([
+                'name' => $examplePeople[$i],
+            ]);
+        }
+
+        $this->command->info('People example created!');
     }
 }
