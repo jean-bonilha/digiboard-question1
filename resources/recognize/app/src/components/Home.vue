aa8f505ab3323aa1e730613ec468311c38b430a3
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2020-03-27 23:07:50 -0400

    Remove metodo detectFace

--- resources/recognize/app/src/components/Home.vue
+++ resources/recognize/app/src/components/Home.vue
@@ -21,64 +21,49 @@
 </template>
 
 <script>
-  import { requestsMixin } from "@/mixins/requestsMixin"
-  import * as faceapi from "face-api.js"
-  const WEIGHTS_URL = "http://localhost/api/weights"
-  export default {
-    name: "home",
-    mixins: [requestsMixin],
-    computed: {
-      images() {
-        return this.$store.state.images
-      }
-    },
-    async beforeMount() {
-      await faceapi.loadTinyFaceDetectorModel(WEIGHTS_URL)
-      await faceapi.loadFaceLandmarkTinyModel(WEIGHTS_URL)
-      await faceapi.loadFaceLandmarkModel(WEIGHTS_URL)
-      await faceapi.loadFaceRecognitionModel(WEIGHTS_URL)
-      await faceapi.loadFaceExpressionModel(WEIGHTS_URL)
-      await faceapi.loadAgeGenderModel(WEIGHTS_URL)
-      await faceapi.loadFaceDetectionModel(WEIGHTS_URL)
-      await this.getAllImages()
-      this.loaded = true
-    },
-    data() {
-      return {
-        form: {},
-        loaded: false
-      }
-    },
-    methods: {
-      async getAllImages() {
-        const { data } = await this.getImages()
-        this.$store.commit("setImages", data)
-        if (this.$refs.file) {
-          this.$refs.file.value = ""
-        }
-      },
-      async detectFace(index) {
-        const input = this.$refs[`photo-${index}`][0]      
-        const options = new faceapi.TinyFaceDetectorOptions({
-          inputSize: 128,
-          scoreThreshold: 0.3
-        })
-        const detections = await faceapi
-          .detectAllFaces(input, options)
-          .withFaceLandmarks()
-          .withFaceExpressions()
-          .withAgeAndGender()
-          .withFaceDescriptors()
-        this.images[index].detections = detections
-        this.getAllImages()
-      }
+import { requestsMixin } from "@/mixins/requestsMixin"
+import * as faceapi from "face-api.js"
+const WEIGHTS_URL = "http://localhost/api/weights"
+export default {
+  name: "home",
+  mixins: [requestsMixin],
+  computed: {
+    images() {
+      return this.$store.state.images
     }
+  },
+  async beforeMount() {
+    await faceapi.loadTinyFaceDetectorModel(WEIGHTS_URL)
+    await faceapi.loadFaceLandmarkTinyModel(WEIGHTS_URL)
+    await faceapi.loadFaceLandmarkModel(WEIGHTS_URL)
+    await faceapi.loadFaceRecognitionModel(WEIGHTS_URL)
+    await faceapi.loadFaceExpressionModel(WEIGHTS_URL)
+    await faceapi.loadAgeGenderModel(WEIGHTS_URL)
+    await faceapi.loadFaceDetectionModel(WEIGHTS_URL)
+    await this.getAllImages()
+    this.loaded = true
+  },
+  data() {
+    return {
+      form: {},
+      loaded: false
+    }
+  },
+  methods: {
+    async getAllImages() {
+      const { data } = await this.getImages()
+      this.$store.commit("setImages", data)
+      if (this.$refs.file) {
+        this.$refs.file.value = ""
+      }
+    },
   }
+}
 </script>
 
 <style>
-  .photo {
-    max-width: 200px;
-    margin-bottom: 10px;
-  }
+.photo {
+  max-width: 200px;
+  margin-bottom: 10px;
+}
 </style>
