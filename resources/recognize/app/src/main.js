83be424ccd71ef37c1713a0221ae36caf9aee95d
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2020-03-27 22:51:34 -0400

    Instala e configura MomentJS e filtro formatDate

--- resources/recognize/app/src/main.js
+++ resources/recognize/app/src/main.js
@@ -2,11 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import moment from 'moment'
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.use(BootstrapVue);
+
+Vue.filter('formatDate', function(value) {
+    if (value) {
+        return moment(String(value)).format('YYYY/MM/DD - hh:mm') + 'Hs'
+    }
+})
 
 Vue.config.productionTip = false
 
