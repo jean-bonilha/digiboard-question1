e40d8f9a1cbe4d5ff67e0e22a96d52fbeb93d1ee
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2020-04-08 09:33:44 -0400

    Adiciona selecionador de imagens para reconhecimento

--- resources/recognize/app/src/components/Train.vue
+++ resources/recognize/app/src/components/Train.vue
@@ -1,38 +1,57 @@
 <template>
   <div class="page">
     <h1 class="text-center">Banco de imagens</h1>
-    <div v-if="loaded">
-      <b-card v-for="(person, index) in people" :key="person.id">
-        <div class="row text-center text-lg-left">
-          <div class="col-md-2">
-            <h4>{{ person.name }}</h4>
-            <p class="sub-title">Criado em: {{ person.created_at | formatDate }}</p>
+    <button class="btn btn-primary" @click="train()">Treinar</button>
+    <picture-input 
+      ref="pictureRecognition"
+      width="400" 
+      height="400" 
+      accept="image/jpeg,image/png"
+      :removable="true"
+      :hideChangeButton="true"
+      :custom-strings="{
+        drag: 'Toque para escolher uma foto',
+        remove: 'Remover a foto'
+      }"
+      margin="16" 
+      >
+    </picture-input>
+      <div v-if="loaded">
+        <b-card class="card-person-images" v-for="person in people" :key="person.id">
+          <div class="row text-center text-lg-left">
+            <div class="col-md-2">
+              <h4>{{ person.name }}</h4>
+              <p class="sub-title">Criado em: {{ person.created_at | formatDate }}</p>
+            </div>
+            <div class="col-md-2" v-for="(photo, index) in person.photos" :key="photo.id">
+              <img :id="photoId(person.name, index)" :src="pathImage(photo.path_storage)" img-alt="Image" class="img-fluid img-thumbnail" :ref="`photo-${index}`" />
+            </div>
           </div>
-          <div class="col-md-2" v-for="photo in person.photos" :key="photo.id">
-            <img :src="pathImage(photo.path_storage)" img-alt="Image" class="img-fluid img-thumbnail" :ref="`photo-${index}`" />
-          </div>
-        </div>
-      </b-card>
-    </div>
-    <div v-else>
-      <p>Carregando dados das imagens...</p>
-    </div>
-    <div v-if="people.length === 0 && loaded === true">
-      <p>Nenhuma imagem foi retornada da consulta!</p>
-    </div>
+        </b-card>
+      </div>
+      <div v-else>
+        <p>Carregando dados das imagens...</p>
+      </div>
+      <div v-if="people.length === 0 && loaded === true">
+        <p>Nenhuma imagem foi retornada da consulta!</p>
+      </div>
   </div>
 </template>
 
 <script>
-import { requestsMixin } from "@/mixins/requestsMixin"
-import * as faceapi from "face-api.js"
+import { requestsMixin } from '@/mixins/requestsMixin'
+import * as faceapi from 'face-api.js'
+import PictureInput from 'vue-picture-input'
 
-const BASE_URL = "http://localhost"
+const BASE_URL = 'http://localhost'
 const API_URL = `${BASE_URL}/api`
-const WEIGHTS_URL = `${API_URL}/weights`
+const MODELS_URL = `${API_URL}/models`
 
 export default {
-  name: "home",
+  name: 'home',
+  components: {
+    PictureInput
+  },
   mixins: [requestsMixin],
   computed: {
     people() {
@@ -40,34 +59,62 @@ export default {
     }
   },
   async beforeMount() {
-    await faceapi.loadTinyFaceDetectorModel(WEIGHTS_URL)
-    await faceapi.loadFaceLandmarkTinyModel(WEIGHTS_URL)
-    await faceapi.loadFaceLandmarkModel(WEIGHTS_URL)
-    await faceapi.loadFaceRecognitionModel(WEIGHTS_URL)
-    await faceapi.loadFaceExpressionModel(WEIGHTS_URL)
-    await faceapi.loadAgeGenderModel(WEIGHTS_URL)
-    await faceapi.loadFaceDetectionModel(WEIGHTS_URL)
+    await faceapi.loadFaceRecognitionModel(MODELS_URL),
+    await faceapi.loadFaceLandmarkModel(MODELS_URL),
+    await faceapi.loadTinyFaceDetectorModel(MODELS_URL),
+    await faceapi.loadFaceExpressionModel(MODELS_URL)
     await this.getAllImages()
     this.loaded = true
   },
   data() {
     return {
       form: {},
-      loaded: false
+      loaded: false,
     }
   },
   methods: {
+    photoId(name, index) {
+      return `photo-${name.toLowerCase()}-${index}`
+    },
     pathImage(parcialPath) {
       return `${BASE_URL}/${parcialPath}`
     },
     async getAllImages() {
       const { data } = await this.getImages()
-      this.$store.commit("setImages", data)
+      this.$store.commit('setImages', data)
       if (this.$refs.file) {
-        this.$refs.file.value = ""
+        this.$refs.file.value = ''
       }
     },
-
+    async train () {
+      const self = this
+      const faces = []
+      await Promise.all(self.people.map(async (person) => {
+        const descriptors = []
+        await Promise.all(person.photos.map(async (photo, index) => {
+          const photoId = self.photoId(person.name, index)
+          const img = document.getElementById(photoId)
+          const options = {
+            detectionsEnabled: true,
+            landmarksEnabled: true,
+            descriptorsEnabled: true,
+            expressionsEnabled: false
+          }
+          const detections = await self.$store.dispatch('getFaceDetections', { canvas: img, options })
+          detections.forEach((d) => {
+            descriptors.push({
+              path: photo,
+              descriptor: d.descriptor
+            })
+          })
+        }))
+        faces.push({
+          person: person.name,
+          descriptors
+        })
+      }))
+      await self.$store.dispatch('saveFaces', faces)
+    },
   }
 }
 </script>
@@ -75,6 +122,12 @@ export default {
 <style>
 .sub-title {
   color: blue;
+}
+.card-person-images {
+  margin-top: 25px;
+}
+.card-body{
+  background-color: #eee;
 }
 .photo {
   max-width: 200px;
--- resources/recognize/app/src/store.js
+++ resources/recognize/app/src/store.js
@@ -1,5 +1,7 @@
-import Vue from "vue"
-import Vuex from "vuex"
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as faceapi from 'face-api.js'
+const axios = require('axios')
 
 Vue.use(Vuex)
 
@@ -7,12 +9,115 @@ export default
 
 new Vuex.Store({
     state: {
-        peopleImages: []
+        peopleImages: [],
+        faces: [],
+        faceMatcher: null,
+
+        useTiny: false,
+
+        detections: {
+          scoreThreshold: 0.5,
+          inputSize: 320,
+          boxColor: 'blue',
+          textColor: 'red',
+          lineWidth: 1,
+          fontSize: 20,
+          fontStyle: 'Georgia'
+        },
+        expressions: {
+          minConfidence: 0.2
+        },
+        landmarks: {
+          drawLines: true,
+          lineWidth: 1
+        },
+        descriptors: {
+          withDistance: false
+        }
     },
     mutations: {
         setImages(state, payload) {
             state.peopleImages = payload
-        }
+        },
+        setFaces(state, payload) {
+            state.faces = payload
+        },
+        setFaceMatcher(state, payload) {
+            state.faceMatcher = payload
+        },
     },
-    actions: {}
+    actions: {
+        async getFaces({ commit }) {
+            const data = await axios.get('faces')
+            commit('setFace', data)
+        },
+        async saveFaces({ commit }, faces) {
+            const { data } = await axios.post('faces', { faces })
+            commit('setFace', data)
+        },
+        async getFaceDetections({ state }, { canvas, options }) {
+            let detections = faceapi
+              .detectAllFaces(canvas, new faceapi.TinyFaceDetectorOptions({
+                scoreThreshold: state.detections.scoreThreshold,
+                inputSize: state.detections.inputSize
+              }))
+
+            if (options && (options.landmarksEnabled || options.descriptorsEnabled)) {
+              detections = detections.withFaceLandmarks(state.useTiny)
+            }
+            if (options && options.expressionsEnabled) {
+              detections = detections.withFaceExpressions()
+            }
+            if (options && options.descriptorsEnabled) {
+              detections = detections.withFaceDescriptors()
+            }
+            detections = await detections
+            return detections
+        },
+        async recognize ({ state }, { descriptor, options }) {
+          if (options.descriptorsEnabled) {
+            const bestMatch = await state.faceMatcher.findBestMatch(descriptor)
+            return bestMatch
+          }
+          return null
+        },
+        draw ({ state }, { canvasDiv, canvasCtx, detection, options }) {
+          let emotions = ''
+          // filter only emontions above confidence treshold and exclude 'neutral'
+          if (options.expressionsEnabled && detection.expressions) {
+            for (const expr in detection.expressions) {
+              if (detection.expressions[expr] > state.expressions.minConfidence && expr !== 'neutral') {
+                emotions += ` ${expr} &`
+              }
+            }
+            if (emotions.length) {
+              emotions = emotions.substring(0, emotions.length - 2)
+            }
+          }
+          let name = ''
+          if (options.descriptorsEnabled && detection.recognition) {
+            name = detection.recognition.toString(state.descriptors.withDistance)
+          }
+
+          const text = `${name}${emotions ? (name ? ' is ' : '') : ''}${emotions}`
+          const box = detection.box || detection.detection.box
+          if (options.detectionsEnabled && box) {
+            // draw box
+            canvasCtx.strokeStyle = state.detections.boxColor
+            canvasCtx.lineWidth = state.detections.lineWidth
+            canvasCtx.strokeRect(box.x, box.y, box.width, box.height)
+          }
+          if (text && detection && box) {
+            // draw text
+            const padText = 2 + state.detections.lineWidth
+            canvasCtx.fillStyle = state.detections.textColor
+            canvasCtx.font = state.detections.fontSize + 'px ' + state.detections.fontStyle
+            canvasCtx.fillText(text, box.x + padText, box.y + box.height + padText + (state.detections.fontSize * 0.6))
+          }
+
+          if (options.landmarksEnabled && detection.landmarks) {
+            faceapi.draw.drawFaceLandmarks(canvasDiv, detection.landmarks, { lineWidth: state.landmarks.lineWidth, drawLines: state.landmarks.drawLines })
+          }
+        },
+    },
 })
